60aacb157ed8a431bb8d556961a1a70f376a36df
rodukov <electroneuphoria@gmail.com>
2022-05-24 22:47:38 +0300

    Arrows

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './css/converter.css'
 import { useState } from 'react';
+import React from 'react';
+import arrow from '../src/assets/arrow-96.png'
 
 function App() {
   // const timer = ms => new Promise(res => setTimeout(res, ms))
@@ -27,29 +29,44 @@ function App() {
   }
 
   const change_input = (event) => { setInput2(parseFloat(event.target.value)*database[selected_1]/database[selected_2]) }
-  const get_selected_1 = (e) => { setSelected_1(e.target.innerText) }
-  const get_selected_2 = (e) => { setSelected_2(e.target.innerText) }
+  const get_selected_1 = (e) => { setSelected_1(e.target.innerText); switch1() }
+  const get_selected_2 = (e) => { setSelected_2(e.target.innerText); switch2() }
 
   function switch1() {
     active_1 = !active_1;
-    if(active_1) { document.getElementsByClassName("dropdown-items")[0].style.opacity = "1"; document.getElementsByClassName("dropdown-items")[0].style.transform = "translateY(7px)"; }
-    else { document.getElementsByClassName("dropdown-items")[0].style.opacity = "0"; document.getElementsByClassName("dropdown-items")[0].style.transform = "translateY(0px)"; }
+    if(active_1) {
+    document.getElementsByClassName("dropdown-items")[0].style.opacity = "1";
+    document.getElementsByClassName("dropdown-items")[0].style.transform = "translateY(7px)";
+    document.getElementsByClassName("arrow1")[0].style.transform = "rotateX(180deg)";
+  }
+    else { document.getElementsByClassName("dropdown-items")[0].style.opacity = "0";
+    document.getElementsByClassName("dropdown-items")[0].style.transform = "translateY(0px)";
+    document.getElementsByClassName("arrow1")[0].style.transform = "rotateX(0deg)";
+  }
   }
   function switch2() {
     active_2 = !active_2;
-    if(active_2) { document.getElementsByClassName("dropdown-items")[1].style.opacity = "1"; document.getElementsByClassName("dropdown-items")[1].style.transform = "translateY(7px)"; }
-    else { document.getElementsByClassName("dropdown-items")[1].style.opacity = "0"; document.getElementsByClassName("dropdown-items")[1].style.transform = "translateY(0px)"; }
+    if(active_2) {
+      document.getElementsByClassName("dropdown-items")[1].style.opacity = "1";
+      document.getElementsByClassName("dropdown-items")[1].style.transform = "translateY(7px)";
+      document.getElementsByClassName("arrow2")[0].style.transform = "rotateX(180deg)";
+    }
+    else { document.getElementsByClassName("dropdown-items")[1].style.opacity = "0";
+    document.getElementsByClassName("dropdown-items")[1].style.transform = "translateY(0px)";
+    document.getElementsByClassName("arrow2")[0].style.transform = "rotateX(0deg)";
+  }
   }
 
   return (
     <div className="main-div">
       <h1 className='default'>Computer Units Converter</h1>
-      <h4 className='default default2'>The most user-friendly open source unit converter. Written in react.js</h4>
+
+      <h4 className='default default2'>The most user-friendly open source unit converter</h4>
       <div className='input-container'>
         <input value={input1} onInput={e => setInput1(e.target.value)} onChange={change_input} placeholder="0" className="input"/>
-        <input value={input2} onInput={e => setInput2(e.target.value)} onChange={change_input} placeholder="0" className="input"/>
+        <input value={input2} readonly="readonly" onInput={e => setInput2(e.target.value)} onChange={change_input} placeholder="0" className="input"/>
         <div className="DropDown">
-          <p className="selectDropDown" onClick={switch1}>{ selected_1 }</p>
+          <div className='basic' onClick={switch1}><p className="selectDropDown">{ selected_1 }</p> <img src={arrow} alt="arrow" className='arrow arrow1'></img></div>
           <div className='dropdown-items'>
             <p className='unit_name' onClick={get_selected_1}>byte</p>
             <p className='unit_name' onClick={get_selected_1}>kilobyte</p>
@@ -63,7 +80,7 @@ function App() {
           </div>
         </div>
         <div className="DropDown">
-          <p className="selectDropDown" onClick={switch2}>{ selected_2 }</p>
+        <div className='basic' onClick={switch2}><p className="selectDropDown">{ selected_2 }</p> <img src={arrow} alt="arrow" className='arrow arrow2'></img></div>
           <div className='dropdown-items'>
             <p className='unit_name' onClick={get_selected_2}>byte</p>
             <p className='unit_name' onClick={get_selected_2}>kilobyte</p>
